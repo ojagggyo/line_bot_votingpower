38556d16b410e72c5dc2c81e0ff33c31df959941
ojagggyo <ojagggyo@gmail.com>
2022-11-27 16:28:35 +0900

    m

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const line =require("@line/bot-sdk");
 const express = require("express");
 require('dotenv').config();
-
 const CONFIG = {
     channelAccessToken: process.env.ACCESS_TOKEN,
     channelSecret: process.env.SECRET_KEY,
@@ -9,17 +8,20 @@ const CONFIG = {
 const PORT = 3001;
 const client = new line.Client(CONFIG);
 express()
-    //.get('/', (req, res) => {res.send('hello world')})
-    .post("/webhook", line.middleware(CONFIG), (req,res) => handleBot(req,res))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    .get('/', 
+        (req, res) => {res.send('hello world')})
+    .post("/webhook", 
+        line.middleware(CONFIG), 
+        (req,res) => handleBot(req,res))
+    .listen(PORT, 
+        () => console.log(`Listening on ${PORT}`));
 
 function handleBot(req,res){
     res.status(200).end();
     req.body.events.map((event) => {
-        console.log(`event.message.text=${event.message.text}`);
         client.replyMessage(event.replyToken,{
             type: 'text',
-            text: `こんにちは！${event.message.text}`
+            text: `${event.message.text}`
         });
     })
 }
